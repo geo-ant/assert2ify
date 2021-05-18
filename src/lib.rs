7eaf58810d2c12e6851790435d8335721efd3602
geo-ant <54497890+geo-ant@users.noreply.github.com>
2021-05-18 22:00:20 +0200

    finalize documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,52 @@
 //! This crates offers the `#[assert2ify]` attribute, a one-line solution to replace standard
-//! library assertions your code with the powerful and more expressive assertions from the
+//! library assertions in your code with the more expressive assertions from the
 //! [assert2](https://crates.io/crates/assert2) crate.
+//!
+//! The attribute not only replaces simple assertions
+//! such as `assert!(...)` or `assert_eq!(...)` but also more complicated constructs like `assert!(matches!(...))` with assertions with way
+//! better error messages.
+//!
 //! # Motivation
-//! The built-in support for testing and assertions in Rust is great, but the standard library
-//! assertions are not super descriptive. Let's say we have some assertion on the length
-//! of a vector like `assert!(my_vector.len() < 5)`. If this assertion panics, it will just tell
-//! you _that_ the condition was violated, but it will not tell you what the actual length of the
+//! Rust's built-in support for tests and assertions is great in so many ways, but the standard library
+//! assertions are lacking in terms of error messages. This is where the [assert2](https://crates.io/crates/assert2) crate comes in handy
+//! because it offers assertions with very helpful error messages.
+//!
+//! Let's say we have some assertion on the length of a vector, such as `assert!(my_vector.len() < 5)`.
+//! If this assertion panics, it will just tell you _that_ the condition was violated,
+//! but it will not tell you what the actual length of the
 //! vector was. To know that, you would have to debug the
-//! test or add extra logging output. Here is where the [assert2](https://crates.io/crates/assert2) crate
+//! test or add extra logging output. The assert2 crate
 //! comes to the rescue, because if we had written the assertion as `assert2::assert!(my_vector.len() < 5)`
-//! then a possible failure would look like this:
+//! then a possible failure would look like this (plus some nice colors):
 //! ```shell
 //! Assertion failed at tests/my_test.rs:107:5:
 //!   assert!( my_vector.len() < 5 )
 //! with expansion:
 //!   7 < 5
 //! ```
-//! Additionally, the [assert2::assert!](assert2::assert) macro adds nicely colored output. Just
-//! stick the `#[assert2ify]` macro into the list of attributes of a test case and take advantage
-//! of more expressive assertions. See what more the [assert2ify](assert2ify) attribute can do for you.
-//! It also replaces `assert!(matches!(...))` expressions by something that has way better error messages.
 //!
+//! You can annotate any function (usually a test case) with `#[assert2ify]` and it
+//! takes care of replacing the assertions inside the functions.
+//!
+//! # Usage
+//! Just annotate any function with the `#[assert2ify]` attribute and have the attribute
+//! replace the assertions in your code with the assertions of the assert2 crate.
+//! It does not only replace simple assertions, but also more complicated constructs like
+//! `assert!(matches!(...))` with something much more helpful. The assertions work as they did before,
+//! but they offer way better error messages.
+//!
+//! Furthermore, the attribute allows you to replace assertions with checks, which do not fail right away but only once the test case is completely
+//! finished. This can help if you want to catch every assertion that fails in a test in a single pass. Just use
+//! the attribute as `#[assert2ify(check)]` for this.
+//!
+//! See what more the [assert2ify](assert2ify) attribute can do for you in the documentation. The docs
+//! also go into detail about the limitations of the attribute.
 
 #[doc(hidden)]
 pub mod assertions;
 
-/// Use this macro to replace the standard library assertions in your test case by sticking this
-/// attribute in the list of attributes of your test function (or any other function that has
-/// assertions inside it).
+/// Annotate your functions (usually test cases) with this attribute and have it replace the
+/// assertions the function with the more powerful assertions from the assert2 crate.
 ///
 /// # Usage
 /// Stick this attribute above any function in which you want the assertions replaced by the
@@ -40,7 +59,9 @@ pub mod assertions;
 /// ### check
 /// The attribute can be used as `#[assert2ify]` without arguments, in which case it will replace
 /// assertions with assertions. If you want to replace assertions with the `check!` macro from the
-/// assert2 crate, use this attribute as `#[assert2ify(check)]`.
+/// assert2 crate, use this attribute as `#[assert2ify(check)]`. In contrast to assertions, the
+/// `check!` macro only fails at the end of the test. This is useful if you want to see every
+/// failing assertion inside a test inside a single pass.
 ///
 /// **Caveat**: `assert!(matches!(...))` expressions will still be replaced by assertions because
 /// there is no `let_check!` in assert2 as it would not make sense.
@@ -82,8 +103,8 @@ pub mod assertions;
 /// ```
 ///
 /// ## `assert!(matches!(...))` Expressions
-/// These statements are used when pattern matching the result of some calculation. Unfortunately
-/// they are pretty useless, because it would be nice to see not only _that_ the match failed
+/// These statements are e.g. used when pattern matching on `Ok` or `Err` variants in unit tests.
+/// Unfortunately they are pretty useless, because it would be nice to see not only _that_ the match failed
 /// but what the type of the matched expression actually was. This is where
 /// [let_assert](https://docs.rs/assert2/latest/assert2/macro.let_assert.html) of the assert2
 /// crate shines. So if we want to test the result of some function `foo` like so
@@ -104,14 +125,14 @@ pub mod assertions;
 /// if statements. This will give you infinitely more helpful messages in case of panics.
 ///
 /// ## Additional Arguments to the Assertions
-/// A format string and additional arguments to the assertions are handled as you would expect
-/// and will produce additional info output in case of panics.
-///
-/// ## `assert!(matches!(...))` Expressions
+/// A format string and all addtional variadic arguments to the assertions are handled as you would expect
+/// and will produce additional info output in case of panics. So an assertion
+/// `assert!(j>5,"wrong number of messages")` will be replaced by an assertion that also carries
+/// the additional info.
 ///
 /// # Limitations and Caveats
 /// The crate traverses the syntax tree given by the contents of the function and replaces the occurrences
-/// of standard library assertions by the assertions of the assert2 crate. For most of the uses
+/// of standard library assertions with the assertions of the assert2 crate. For most of the uses
 /// cases this will be fine, since assertions inside loops, closures, if-statements, etc. will be
 /// replaced
 ///
